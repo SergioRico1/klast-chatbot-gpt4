58718c76d975167da804b41e87c815f9fb4d53ee
SergioRico1 <51226753+SergioRico1@users.noreply.github.com>
2023-05-05 12:06:36 +0000

    disclamer v2

--- pages/index.tsx
+++ pages/index.tsx
@@ -318,8 +318,8 @@ export default function Home() {
         </div>
         <footer className="m-auto p-4">
         <div className={styles.infomsg}>
-        <i class="fa fa-info-circle"></i>
-        &nbsp;Estoy aprendiendo, así que puedo contener errores.
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-.001 5.75c.69 0 1.251.56 1.251 1.25s-.561 1.25-1.251 1.25-1.249-.56-1.249-1.25.559-1.25 1.249-1.25zm2.001 12.25h-4v-1c.484-.179 1-.201 1-.735v-4.467c0-.534-.516-.618-1-.797v-1h3v6.265c0 .535.517.558 1 .735v.999z"/></svg>
+        &nbsp;Si alguna respuesta no es lo que buscas, es por que estoy aprendiendo.
         </div>
         </footer>
       </Layout>
